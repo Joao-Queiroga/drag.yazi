e4ec2d4d557046aef140ffe76ead4102d3bac38b
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-07-20 15:33:42 -0300

    feat: adding MacOS support via mktemp

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local ya, cx, Command = ya, cx, Command
+
 ---@param s string
 local function fail(s, ...)
 	ya.notify({ title = "ripdrag", content = string.format(s, ...), timeout = 3, level = "error" })
@@ -15,16 +17,51 @@ local selected_files = ya.sync(function()
 	return paths
 end)
 
+---@return string
+local function get_os()
+	return (select(1, Command("uname"):stdout(Command.PIPED):output()).stdout or ""):lower()
+end
+
 return {
 	entry = function()
 		local files = selected_files()
-		local cmd = Command("ripdrag"):args({ "-a", "-x" }):args(files)
+		local cmd
+
+
+        if not #files then
+            return
+        end
+
+		if get_os():match("^darwin") then
+            local quoted_files, file_str = {}, ""
+            for _, file in ipairs(files) do
+                quoted_files[#quoted_files+1] = "'" .. file:gsub("'", "'\\''") .. "'"
+            end
+
+            file_str = table.concat(quoted_files, " ")
+
+			local bash_cmd = string.format(
+			[[
+            BASE_TMP_DIR="${TMPDIR:-/tmp}"
+            TARGET_DIR="${BASE_TMP_DIR}/yazi-%s"
+            [ -d "$TARGET_DIR" ] && rm -rf "$TARGET_DIR"/* || mkdir -p "$TARGET_DIR"
+            cp -R %s "$TARGET_DIR" && open "$TARGET_DIR"
+            ]],
+				os.getenv("YAZI_ID") or "000000",
+                file_str
+			)
+			cmd = Command("bash"):arg({ "-c", bash_cmd })
+		else
+			cmd = Command("ripdrag"):arg({ "-a", "-x" }):arg(files)
+		end
+
 		local child, err = cmd:spawn()
 		if not child then
 			fail("Spawn `ripdrag` failed with error code %s.", err)
 			return
 		end
-		local output, err = child:wait_with_output()
+        local output
+		output, err = child:wait_with_output()
 		if not output then
 			fail("Cannot read `ripdrag` output, error code %s", err)
 		elseif not output.status.success and output.status.code ~= 131 then
